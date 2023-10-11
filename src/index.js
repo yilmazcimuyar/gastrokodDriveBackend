6d0f698c9c0e858817dbeb77d8960cdb4380229c
Yılmaz Uyar <yilmaz.u.237@gmail.com>
2023-10-11 16:23:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -48,6 +48,6 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("App is listening on port 5000");
+app.listen(6000, () => {
+  console.log("App is listening on port 6000");
 });
